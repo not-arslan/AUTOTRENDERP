234d65dbc9881351fb9082a4d584e4ac0f1300d3
not-arslan <arslan.2013.01.02@gmail.com>
2025-07-09 23:11:34 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def login_user():
+    st.sidebar.subheader("🔐 Login")
+    email = st.sidebar.text_input("Email")
+    password = st.sidebar.text_input("Password", type="password")
+    login = st.sidebar.button("Login")
+    
+    if login and email == "admin@gmail.com" and password == "Anis@1978":
+        return True
+    elif login:
+        st.sidebar.error("Invalid credentials")
+    
+    return False
