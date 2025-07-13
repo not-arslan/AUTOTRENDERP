7604a650cf64efb7290dfd033ea412d407789ec9
not-arslan <arslan.2013.01.02@gmail.com>
2025-07-13 17:37:58 +0530

    Delete components directory

--- components/charts.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def render_charts():
-    st.subheader("📉 Crude Oil Charts")
-    st.info("Charts will be embedded from TradingView or Plotly soon.")
-    st.markdown("💡 If you want, you can embed TradingView widgets using `st.components.v1.html()`.")
